07c0773663937656371ec3a94e8757e2699210bf
bj911 <bj911@ukr.net>
2020-09-22 16:16:29 +0300

    fix

--- db/migrate/20200922122724_add_public_to_d_meters.rb
+++ db/migrate/20200922122724_add_public_to_d_meters.rb
@@ -2,6 +2,4 @@ class AddPublicToDMeters < ActiveRecord::Migration[6.0]
   def change
     add_column :d_meters, :public, :boolean
   end
-
-  DMeter.first.update(public: true) if DMeter.first.present?
 end
